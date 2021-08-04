ee63267cf77c9105b6e92161c1823d3f118543b3
Adam <narozniak.adam4@gmail.com>
2021-08-04 09:59:15 +0200

    Second version working

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ import numpy as np
 from numpy.random import default_rng
 from datetime import datetime
 import os
+import datetime
+
 
 def make_parser():
     parser = argparse.ArgumentParser(description="Tool to detect FMEL housing website changes")
@@ -23,6 +25,7 @@ def make_parser():
 
 
 def check_screen_size_config_validity(clicks_coordinates):
+    print(clicks_coordinates)
     assert clicks_coordinates[0]["x"] < clicks_coordinates[1]["x"]
     assert clicks_coordinates[0]["y"] < clicks_coordinates[1]["y"]
     print(f"Click configs are valid {clicks_coordinates}")
@@ -45,9 +48,9 @@ def get_screenshot_coordianates():
 
 
 if __name__ == '__main__':
-    time.sleep(2)
+    time.sleep(10)
     start_time = time.process_time()
-
+    eight_o_clock = datetime.datetime.now().replace(hour=8, minute=0)
     parser = make_parser()
     args = parser.parse_args()
     output_directory = args.output_directory
@@ -58,8 +61,18 @@ if __name__ == '__main__':
         # take user input to specify points
         print("Please click at the place that will be top left corner. "
               "Then click at the place that will be bottom right corner.")
+        os.system("""spd-say "Please click at the place that will be top left corner. 
+              Then click at the place that will be bottom right corner." -w""")
         clicks_coordinates = get_screenshot_coordianates()
         print(clicks_coordinates)
+        with open("/home/adam/Documents/projects/python/FMEL_housing/resources/screen_size_config.json", "w") as f:
+            json_dump= {
+                "top_left":
+                    {"x": clicks_coordinates[0]["x"], "y": clicks_coordinates[0]["y"]},
+                "bottom_right":
+                    {"x": clicks_coordinates[1]["x"], "y": clicks_coordinates[1]["y"]}
+            }
+            json.dump(json_dump, f, indent=4)
     else:
         # get the config from a file
         screen_config = read_screen_size_config(json_path)
@@ -83,27 +96,33 @@ if __name__ == '__main__':
     print(numpy_screenshot)
     rng = default_rng()
     while True:
+        now = datetime.datetime.now()
         pyautogui.click(refresh_button_center_location)
         noise = rng.normal(loc=4, scale=2)
-        sleep_time = max(0, 120 + noise)
+        sleep_time = max(0, 30 + noise)
         time.sleep(sleep_time)
-        new_screenshot = pyautogui.screenshot().crop((clicks_coordinates[0]["x"], clicks_coordinates[0]["y"],
-                                                      clicks_coordinates[1]["x"], clicks_coordinates[1]["y"]))
-        numpy_new_screenshot = np.array(new_screenshot)
-        if not (numpy_screenshot == numpy_new_screenshot).all():
-            # first try to log in again
-            # TODO: implement logging in
+        if now > eight_o_clock:
+            new_screenshot = pyautogui.screenshot().crop((clicks_coordinates[0]["x"], clicks_coordinates[0]["y"],
+                                                          clicks_coordinates[1]["x"], clicks_coordinates[1]["y"]))
+            numpy_new_screenshot = np.array(new_screenshot)
+            if not (numpy_screenshot == numpy_new_screenshot).all():
+                # first try to log in again
+                # TODO: implement logging in
 
-            # 1a. If after refresh the page is the logging page then log in
-            if False:#  (numpy_new_screenshot == numpy_logging_page).all():
-                log_in_button_center_location = pyautogui.locateCenterOnScreen("./resources/log_in_button.png")
-                pyautogui.click(log_in_button_center_location)
-            # 1b. Else (there has been a new free place) => make a screenshot and start playing the sound
+                # 1a. If after refresh the page is the logging page then log in
+                if False:  # (numpy_new_screenshot == numpy_logging_page).all():
+                    log_in_button_center_location = pyautogui.locateCenterOnScreen("./resources/log_in_button.png")
+                    pyautogui.click(log_in_button_center_location)
+                # 1b. Else (there has been a new free place) => make a screenshot and start playing the sound
+                else:
+                    new_place_screenshot = pyautogui.screenshot(output_directory / "new_place.png")
+                    select_button_center_location = pyautogui.locateCenterOnScreen("./resources/select_button.png",
+                                                                                   confidence=0.90)
+                    pyautogui.click(select_button_center_location)
+                    print(
+                        f"The page has changed at: {datetime.datetime.now()}.\nIt was after {10 * (time.process_time() - start_time)} seconds since the program got started")
+                    while True:
+                        os.system("""spd-say "Possibly there is a new free place at FMEL." -w""")
+                        time.sleep(5)
             else:
-                new_place_screenshot = pyautogui.screenshot(output_directory/"new_place.png")
-                print(
-                    f"The page has changed at: {datetime.now()}.\nIt was after {10 * (time.process_time() - start_time)} seconds since the program got started")
-                while True:
-                    os.system("""spd-say "Possibly there is a new free place at FMEL." -w""")
-        else:
-            print("The site is the same")
+                print("The site is the same")
