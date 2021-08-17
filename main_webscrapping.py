27a7f2da2cafa091785f3e5a580741668cc3144e
Adam Narozniak <narozniak.adam4@gmail.com>
2021-08-17 02:15:07 +0200

    Add screenshots saving, and various notification time

--- main_webscrapping.py
+++ main_webscrapping.py
@@ -1,14 +1,15 @@
 import argparse
+import datetime
 import json
 import os
+import pathlib
 import platform
 import time
-from datetime import datetime
-import yagmail
 
-import selenium.common.exceptions
-from selenium import webdriver
 import selenium
+import selenium.common.exceptions
+import yagmail
+from selenium import webdriver
 
 FMEL_CREDENTIALS_CONFIGURATION_PATH = "./resources/credentials_config.json"
 EMAIL_CREDENTIALS_CONFIGURATION_PATH = "./resources/email_credentials_config.json"
@@ -75,10 +76,15 @@ def get_credentials(credentials_configuration_path):
     return username, password
 
 
-def sound_notification():
+def sound_notification(duration):
     system_name = platform.system()
     if system_name == "Darwin" or system_name == "Linux":
-        os.system("""say "Possibly there is a new free place at FMEL." """)
+        delta = datetime.timedelta(seconds=duration)
+        begin_time = datetime.datetime.now()
+        now = datetime.datetime.now()
+        while now < begin_time + delta:
+            os.system("""say "Possibly there is a new free place at FMEL." """)
+            now = datetime.datetime.now()
     else:
         raise Exception(f"System not supported")
 
@@ -89,11 +95,27 @@ def set_up_email(email_username, email_password):
     return yag
 
 
+def prepare_dirs():
+    page_sources = pathlib.Path("./page_sources")
+    page_sources.mkdir(exist_ok=True)
+    ps_after_book_was_clicked = page_sources / "after_book_was_clicked"
+    ps_after_book_was_clicked.mkdir(exist_ok=True)
+    ps_after_select_was_clicked = page_sources / "after_select_was_clicked"
+    ps_after_select_was_clicked.mkdir(exist_ok=True)
+    screenshots = pathlib.Path("./screenshots")
+    screenshots.mkdir(exist_ok=True)
+    sc_after_book_was_clicked = screenshots / "after_book_was_clicked"
+    sc_after_book_was_clicked.mkdir(exist_ok=True)
+    sc_after_select_was_clicked = screenshots / "after_select_was_clicked"
+    sc_after_select_was_clicked.mkdir(exist_ok=True)
+
+
 def main():
     parser = make_parser()
     args = parser.parse_args()
     mode = args.mode
     date = args.date
+    prepare_dirs()
     fmel_username, fmel_password = get_credentials(FMEL_CREDENTIALS_CONFIGURATION_PATH)
     if mode == "email":
         email_username, email_password = get_credentials(EMAIL_CREDENTIALS_CONFIGURATION_PATH)
@@ -101,9 +123,10 @@ def main():
         to = email_username
         subject = "Possibly a new free place at FMEL"
     driver = webdriver.Chrome()
+    driver.maximize_window()
     was_outside_working_hours = True
     while True:
-        now = datetime.now().hour
+        now = datetime.datetime.now().hour
         if True:  # 6 <= now < 18:
             # log in for the first time/ you were in inactive hours so probably log in is needed
             if was_outside_working_hours:
@@ -119,7 +142,7 @@ def main():
                 continue
             # there is no string indicating no places available
             if refreshed_page.find("Please check regularly for new availabilities") == -1:
-                change_time = datetime.now()
+                change_time = datetime.datetime.now()
                 print(f"page has changed at {change_time}!")
                 with open(f"./page_sources/ps_for_{date.replace('/', '.')}_{change_time}.txt", "w") as f:
                     f.write(driver.page_source)
@@ -127,14 +150,29 @@ def main():
                                                       "div[1]/section/form/div/div[2]/div[2]/div/div[2]/button")
                 driver.execute_script('arguments[0].scrollIntoView(true);', target)
                 driver.save_screenshot(f"./screenshots/fmel_{date.replace('/', '.')}_{change_time}.png")
+                # click select button
                 target.click()
-                driver.save_screenshot(f"./screenshots/selected_fmel_{date.replace('/', '.')}_{change_time}.png")
-                with open(f"./page_sources/selected_ps_for_{date.replace('/', '.')}_{change_time}.txt", "w") as f:
+                driver.implicitly_wait(0.3)
+                with open(f"./page_sources/after_select_was_clicked/ps_for_{date.replace('/', '.')}_{change_time}.txt",
+                          "w") as f:
                     f.write(driver.page_source)
+                driver.save_screenshot(
+                    f"./screenshots/after_select_was_clicked/fmel_{date.replace('/', '.')}_{change_time}.png")
+                # click book now
+                book_button = driver.find_element_by_xpath("/html/body/div[2]/section[1]/div/article/div/div/div/"
+                                                           "section/div[1]/section/form/div/div[2]/div[2]/div[1]/"
+                                                           "div[1]/div[3]/button[1]")
+                book_button.click()
+                driver.implicitly_wait(0.3)
+                with open(f"./page_sources/after_book_was_clicked/ps_for_{date.replace('/', '.')}_{change_time}.txt",
+                          "w") as f:
+                    f.write(driver.page_source)
+                driver.save_screenshot(
+                    f"./screenshots/after_book_was_clicked/fmel_{date.replace('/', '.')}_{change_time}.png")
                 # notify about that
                 if mode == "voice":
                     while True:
-                        sound_notification()
+                        sound_notification(15)
                 elif mode == "email":
                     # send an email and go back to checking availability after 5 minutes
                     yag.send(to, subject, f"Check this page for date {date}:\n{driver.current_url}")
